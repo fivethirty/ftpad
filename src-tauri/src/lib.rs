3e876332d73a860ff89f9342db8c3cafdfb119ab
Mike Mellenthin <mike.mellenthin@gmail.com>
2026-05-21 22:56:57 -0400

    second commit

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,9 @@
 use tauri::{
+    menu::{Menu, MenuItem},
     tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
     Manager,
 };
 use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
-
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -19,54 +19,65 @@ pub fn run() {
                 let _ = window.set_title("");
 
                 #[cfg(target_os = "macos")]
-                window.with_webview(|webview| unsafe {
-                    use cocoa::base::{id, YES};
-                    use objc::{msg_send, sel, sel_impl};
+                window
+                    .with_webview(|webview| unsafe {
+                        use objc2_app_kit::{NSWindow, NSWindowButton};
 
-                    let ns_window = webview.ns_window() as id;
-                    for btn_index in [0_u64, 1_u64, 2_u64] {
-                        let btn: id = msg_send![ns_window, standardWindowButton: btn_index];
-                        if !btn.is_null() {
-                            let _: () = msg_send![btn, setHidden: YES];
+                        let ns_window = &*(webview.ns_window() as *mut NSWindow);
+                        for btn in [
+                            NSWindowButton::CloseButton,
+                            NSWindowButton::MiniaturizeButton,
+                            NSWindowButton::ZoomButton,
+                        ] {
+                            if let Some(btn) = ns_window.standardWindowButton(btn) {
+                                btn.setHidden(true);
+                            }
                         }
-                    }
-                }).ok();
+                    })
+                    .ok();
 
                 let win = window.clone();
-                window.on_window_event(move |event| {
-                    match event {
-                        tauri::WindowEvent::CloseRequested { api, .. } => {
-                            api.prevent_close();
-                            let _ = win.hide();
-                        }
-                        tauri::WindowEvent::Focused(false) => {
-                            let _ = win.hide();
-                        }
-                        _ => {}
+                window.on_window_event(move |event| match event {
+                    tauri::WindowEvent::CloseRequested { api, .. } => {
+                        api.prevent_close();
+                        let _ = win.hide();
                     }
+                    tauri::WindowEvent::Focused(false) => {
+                        let _ = win.hide();
+                    }
+                    _ => {}
                 });
             }
 
-            let shortcut = Shortcut::new(
-                Some(Modifiers::CONTROL | Modifiers::SHIFT),
-                Code::Space,
-            );
+            let shortcut = Shortcut::new(Some(Modifiers::CONTROL | Modifiers::SHIFT), Code::Space);
             let handle = app.handle().clone();
-            app.global_shortcut().on_shortcut(shortcut, move |_app, _shortcut, event| {
-                if event.state != ShortcutState::Pressed { return; }
-                if let Some(window) = handle.get_webview_window("main") {
-                    if window.is_visible().unwrap_or(false) {
-                        let _ = window.hide();
-                    } else {
-                        let _ = window.show();
-                        let _ = window.set_focus();
+            app.global_shortcut()
+                .on_shortcut(shortcut, move |_app, _shortcut, event| {
+                    if event.state != ShortcutState::Pressed {
+                        return;
                     }
-                }
-            })?;
+                    if let Some(window) = handle.get_webview_window("main") {
+                        if window.is_visible().unwrap_or(false) {
+                            let _ = window.hide();
+                        } else {
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                })?;
+
+            let quit = MenuItem::with_id(app, "quit", "Quit ftpad", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&quit])?;
 
             let tray = TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
                 .icon_as_template(true)
+                .menu(&menu)
+                .on_menu_event(|app, event| {
+                    if event.id() == "quit" {
+                        app.exit(0);
+                    }
+                })
                 .on_tray_icon_event(|tray, event| {
                     if let TrayIconEvent::Click {
                         button: MouseButton::Left,
